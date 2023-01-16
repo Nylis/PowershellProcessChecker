a3dd53d5014cbd1abc2feafa6d88427afb32828c
Nylis <cgj.nijland@gmail.com>
2023-01-16 17:24:43 +0100

    before remote support

--- Processchecker.ps1
+++ Processchecker.ps1
@@ -1,5 +1,11 @@
+# Please check if you are using a recent powershell version before performing this script
+
 $PINGATTEMPTS = 1
 $NULL_STRING = ""
+$INTERVAL = 5 # seconds
+$WHITELIST_FILE = "Whitelist.txt"
+$CURRENT_PROCESSES_FILE = "current-processes.txt"
+$BLACKLIST_FILE = "Signaleringen.txt"
 $counter = 0
 
 function endScript
@@ -14,11 +20,12 @@ function serverName
     $SERVER_ADDRESS = Read-Host "Servername"
 
     if ($NULL_STRING -eq $SERVER_ADDRESS) {
-        Write-Host "User did not fill string. Defaulting to win2022serv.nijland.lan..."
         $SERVER_ADDRESS = "win2022serv.nijland.lan"
+        Write-Host "User did not fill string. Defaulting to $SERVER_ADDRESS..."
     }
 
     while ($counter -lt $PINGATTEMPTS) {
+        Write-Host "Performing ping test $counter..."
         $ping_status = (Test-Connection $SERVER_ADDRESS -Count 1).Status
         if ($ping_status -ne "Success") {
             Write-Host "Ping to system failed. Check your network connection and try again."
@@ -43,15 +50,24 @@ function duration {
     return $DURATION_SECONDS
 }
 
-$SERVER_ADDRESS = serverName
-$DURATION_SECONDS = duration
+function instantWhitelist {
+    $instant_whitelist = Read-Host "Whitelist all PIDs during first cycle (y/n)?"
+    if ($instant_whitelist -eq "y") {
+        $instant_whitelist = $true
+    }
+    else {
+        $instant_whitelist = $false
+    }
+
+    return $instant_whitelist
+}
 
 function insertUserInput {
-    Clear-Content Signaleringen.txt
-    Add-Content Signaleringen.txt "Address: $SERVER_ADDRESS"
-    Add-Content Signaleringen.txt "Duration: $DURATION_SECONDS seconds"
-    Add-Content Signaleringen.txt "----"
-    Add-Content Signaleringen.txt ""
+    Clear-Content $BLACKLIST_FILE
+    Add-Content $BLACKLIST_FILE "Address: $SERVER_ADDRESS"
+    Add-Content $BLACKLIST_FILE "Duration: $DURATION_SECONDS seconds"
+    Add-Content $BLACKLIST_FILE "----"
+    Add-Content $BLACKLIST_FILE ""
 
     Write-Host "Target server: $SERVER_ADDRESS"
     Write-Host "Duration: $DURATION_SECONDS"
@@ -59,46 +75,69 @@ function insertUserInput {
 
 function mainLoop {
     $current_duration = 0
-    
-    while ($current_duration -lt $DURATION_SECONDS) {
-        Start-Sleep 5
+    $cycle = 0
 
-        Get-Process | Format-Table -HideTableHeaders Id | Out-File current-processes.txt
-        $current_processes = Get-Content current-processes.txt
+    while ($current_duration -lt $DURATION_SECONDS) {
+        $timer = $INTERVAL
+        while ($counter -lt $INTERVAL) {
+            Write-Host "Performing next cycle in $timer seconds..." 
+            $dots += ". " 
+            $counter += 1
+            $timer -= 1
+            Start-Sleep 1
+        }
+        $counter = 0
+
+        Get-Process | Format-Table -HideTableHeaders Id | Out-File $CURRENT_PROCESSES_FILE
+        $current_processes = Get-Content $CURRENT_PROCESSES_FILE
         foreach ($working_pid in $current_processes) {
             $working_pid = $working_pid.replace(' ', '')
 
-            $current_whitelist = Get-Content Whitelist.txt
+            $current_whitelist = Get-Content $WHITELIST_FILE
             foreach ($whitelist_pid in $current_whitelist) {
                 if ($whitelist_pid -eq $working_pid) {
                     $in_whitelist = $true
                 }
             }
 
-            $current_blacklist = Get-Content Signaleringen.txt
+            $current_blacklist = Get-Content $BLACKLIST_FILE
             foreach ($blacklist_pid in $current_blacklist) {
                 if ($blacklist_pid -eq $working_pid) {
                     $in_blacklist = $true
                 }
             }
         
-            if ($in_whitelist -ne $true -and $in_blacklist -ne $true) {
+            if ($in_whitelist -ne $true -and $in_blacklist -ne $true -and $instant_whitelist -ne $true) {
                 $answer = Read-Host "PID $working_pid is not in whitelist or blacklist. Add to whitelist (y/n)?"
                 if ($answer -eq "y") {
-                    Add-Content Whitelist.txt "$working_pid"
+                    Write-Host "Added $working_pid to $WHITELIST_FILE"
+                    Add-Content $WHITELIST_FILE "$working_pid"
                 }
                 else {
-                    Add-Content Signaleringen.txt "$working_pid"
+                    Write-Host "Added $working_pid to $BLACKLIST_FILE"
+                    Add-Content $BLACKLIST_FILE "$working_pid"
                 }
+            }
+
+            if ($instant_whitelist -eq $true) {
+                Write-Host "Added $working_pid to $WHITELIST_FILE"
+                Add-Content $WHITELIST_FILE "$working_pid"
             }
 
             $in_blacklist = $false
             $in_whitelist = $false
         }
 
-        $current_duration += 5
+        $current_duration += $INTERVAL
+        $instant_whitelist = $false
+        $cycle += 1
+        Write-Host "Cycle $cycle completed."
     }
 }
+
+$SERVER_ADDRESS = serverName
+$DURATION_SECONDS = duration
+$instant_whitelist = instantWhitelist
 
 insertUserInput
 mainLoop
